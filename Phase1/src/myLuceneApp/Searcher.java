f20d05199461876504162369252605b0916b6f99
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-04-29 19:56:50 +0300

    Searcher has error with WildcardQuery

--- Phase1/src/myLuceneApp/Searcher.java
+++ Phase1/src/myLuceneApp/Searcher.java
@@ -3,7 +3,10 @@ package myLuceneApp;
 // tested for lucene 7.7.2 and jdk13
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.nio.file.Paths;
+import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
@@ -18,11 +21,17 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.FSDirectory;
 
+import txtparsing.Doc;
+import txtparsing.Question;
+import txtparsing.TXTParsing;
+
 public class Searcher {
     
     public Searcher(){
         try{
             String indexLocation = ("index"); //define where the index is stored (from IndexerDemo!!!)
+            String queriesName = "docs/CISI.QRY";
+            String resultsName = "docs/resultsCISI.txt";
             String field = "contents"; //define which field will be searched            
             
             //Access the index using indexReaderFSDirectory.open(Paths.get(index))
@@ -30,8 +39,27 @@ public class Searcher {
             IndexSearcher indexSearcher = new IndexSearcher(indexReader); //Creates a searcher searching the provided index, Implements search over a single IndexReader.
             indexSearcher.setSimilarity(new BM25Similarity());
             
-            //Search the index using indexSearcher
-            search(indexSearcher, field);
+            File resultsFile = new File(resultsName);
+            resultsFile.createNewFile();
+            FileWriter writer = new FileWriter(resultsFile);
+            
+            List<Question> questions = TXTParsing.parseQueries(queriesName);
+        	System.out.println("Questions " + questions.size());
+
+
+            for (Question q : questions){
+
+            	//Search the index using indexSearcher
+            	TopDocs results = search(indexSearcher, field, q.getQuery());
+            	ScoreDoc[] hits = results.scoreDocs;
+            	for(ScoreDoc hit: hits) {
+            		Document hitdoc = indexSearcher.doc(hit.doc);
+            		writer.write(q.getId() + " Q0 " + hitdoc.get("id") + " 0 " + hit.score + " STANDARD\n");
+            		System.out.print(q.getId() + " Q0 " + hitdoc.get("id") + " 0 " + hit.score + " STANDARD\n");
+            	}
+            }
+            writer.close();
+            
             
             //Close indexReader
             indexReader.close();
@@ -43,7 +71,7 @@ public class Searcher {
     /**
      * Searches the index given a specific user query.
      */
-    private void search(IndexSearcher indexSearcher, String field){
+    private TopDocs search(IndexSearcher indexSearcher, String field, String question){
         try{
             // define which analyzer to use for the normalization of user's query
             Analyzer analyzer = new EnglishAnalyzer();
@@ -51,34 +79,27 @@ public class Searcher {
             // create a query parser on the field "contents"
             QueryParser parser = new QueryParser(field, analyzer);
             
-            // read user's query from stdin
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Enter query or 'q' to quit: ");
-            System.out.print(">>");
-            String line = br.readLine();
-            while(line!=null && !line.equals("") && !line.equalsIgnoreCase("q")){
-                // parse the query according to QueryParser
-                Query query = parser.parse(line);
-                System.out.println("Searching for: " + query.toString(field));
-                
-                // search the index using the indexSearcher
-                TopDocs results = indexSearcher.search(query, 100);
-                ScoreDoc[] hits = results.scoreDocs;
-                long numTotalHits = results.totalHits;
-                System.out.println(numTotalHits + " total matching documents");
+            Query query = parser.parse(question);
+            System.out.println("Searching for: " + query.toString(field));
+            
+            // search the index using the indexSearcher
+            TopDocs results = indexSearcher.search(query, 100);
+            
+        	/*ScoreDoc[] hits = results.scoreDocs;
+        long numTotalHits = results.totalHits;
+        System.out.println(numTotalHits + " total matching documents");
 
-                //display results
-                for(int i=0; i<hits.length; i++){
-                    Document hitDoc = indexSearcher.doc(hits[i].doc);
-                    System.out.println("\tScore "+hits[i].score +"\ttitle="+hitDoc.get("title")+"\tcaption:"+hitDoc.get("caption")+"\tmesh:"+hitDoc.get("mesh"));
-                }
-                
-                System.out.println("Enter query or 'q' to quit: ");
-                System.out.print(">>");
-                line = br.readLine();
-            }
+        //display results
+        for(int i=0; i<hits.length; i++){
+            Document hitDoc = indexSearcher.doc(hits[i].doc);
+            System.out.println("\tScore "+hits[i].score +"\ttitle="+hitDoc.get("title")+"\tcaption:"+hitDoc.get("caption")+"\tmesh:"+hitDoc.get("mesh"));
+        }*/
+            
+            return results;
+
         } catch(Exception e){
             e.printStackTrace();
+            return null;
         }
     }
 
